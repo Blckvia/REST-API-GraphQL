c0703d9a1cef64e349156898dd30f363e2f1f2af
blckvia <a.vyatkinn@icloud.com>
2022-11-25 18:03:23 +0500

    signup and user model added

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,12 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
 const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
+    const currentPage = req.query.page || 1;
+    const perPage = 2;
+    let totalItems;
     Post.find()
+        .countDocuments()
+        .then((count) => {
+            totalItems = count;
+            return Post.find()
+                .skip((currentPage - 1) * perPage)
+                .limit(perPage);
+        })
         .then((posts) => {
             res.status(200).json({
                 message: 'Fetched posts successfully.',
                 posts: posts,
+                totalItems: totalItems,
             });
         })
         .catch((err) => {
@@ -16,6 +30,7 @@ exports.getPosts = (req, res, next) => {
             next(err);
         });
 };
+
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -24,10 +39,6 @@ exports.createPost = (req, res, next) => {
         );
         error.statusCode = 422;
         throw error;
-        // return res.status(422).json({
-        //     message: 'Validation failed, entered data is incorrect.',
-        //     errors: errors.array(),
-        // });
     }
     if (!req.file) {
         const error = new Error('No image provided.');
@@ -77,4 +88,84 @@ exports.getPost = (req, res, next) => {
             }
             next(err);
         });
+};
+
+exports.updatePost = (req, res, next) => {
+    const postId = req.params.postId;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error(
+            'Validation failed, entered data is incorrect.'
+        );
+        error.statusCode = 422;
+        throw error;
+    }
+    const title = req.body.title;
+    const content = req.body.content;
+    let imageUrl = req.body.image;
+    if (req.file) {
+        imageUrl = req.file.path;
+    }
+    if (!imageUrl) {
+        const error = new Error('No file picked.');
+        error.statusCode = 422;
+        throw error;
+    }
+    Post.findById(postId)
+        .then((post) => {
+            if (!post) {
+                const error = new Error('Could not find post.');
+                error.statusCode = 404;
+                throw error;
+            }
+            if (imageUrl !== post.imageUrl) {
+                clearImage(post.imageUrl);
+            }
+            post.title = title;
+            post.imageUrl = imageUrl;
+            post.content = content;
+            return post.save();
+        })
+        .then((result) => {
+            res.status(200).json({ message: 'Post updated!', post: result });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
+
+exports.deletePost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then((post) => {
+            if (!post) {
+                const error = new Error('Could not find a post');
+                error.statusCode = 404;
+                throw error;
+            }
+            // Check logged user
+            clearImage(post.imageUrl);
+            return Post.findByIdAndRemove(postId);
+        })
+        .then((result) => {
+            console.log(result);
+            res.status(200).json({ message: 'Deleted post.' });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
+
+// I want to trigger that clearImage func when ever
+// I uploaded a new image
+const clearImage = (filePath) => {
+    filePath = path.join(__dirname, '..', filePath);
+    // unlink func created to delete file
+    fs.unlink(filePath, (err) => console.log(err));
 };
